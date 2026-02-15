8324248f3d6f49a70867ad3ea59a13ddf488dcae
Your Name <you@example.com>
2026-02-15 05:40:03 +0200

    Add images

--- app/Models/requirements.php
+++ app/Models/requirements.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class requirements extends Model
+class Requirements extends Model
 {
     use HasFactory;
     protected $fillable = ['key','label','type'];
